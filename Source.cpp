6a95121f3fd88499fcb07ac507f61337e19116ed
Anton Galkovsky <galkovsky_anton@mail.ru>
2018-04-08 18:03:31 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,379 +1,222 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
-#include <vector>
-#include <string>
-#include <set>
-
+#include <algorithm>
+#include <ctime>
 using namespace std;
-//Структура конкретизаций. Содержит конкретизируемую букву (letter) и её позицию в слове (number)
-struct Conc {
-	int number;
-	char letter;
-
-	Conc() {}
-
-	Conc(int n, char l) {
-		number = n;
-		letter = l;
-	}
-
-	bool operator<(const Conc conc) const {
-		if (number != conc.number)
-			return number < conc.number;
-		return letter < conc.letter;
-	}
+struct Cell {
+	int x, y;
 };
-
-struct Node;
-//Структура рёбер. Содержит указатель на вершину (*node) и соответствующую конкретизацию (conc)
-struct Rib {
-	Conc conc;
-	Node *node;
-
-	Rib() {}
-
-	Rib(Conc c, Node *n) {
-		conc = c;
-		node = n;
-	}
-
-	bool operator<(const Rib rib) const {
-		return conc < rib.conc;
-	}
+struct Row {
+	long long spot, info;
 };
-//Структура узлов. Содержит набор слов, соответствующих узлу (words), 
-//набор исходящих из узла и ведущих в другие узлы рёбер (ribs) и набор петель узла (loops)
-struct Node {
-	set<string> words;
-	set<Rib> ribs;
-	set<Conc> loops;
-
-	Node() {}
-
-	Node(string str) {
-		words.insert(str);
-		for (unsigned i = 0; i < str.size(); i++)
-			loops.insert(Conc(i, str[i]));
-	}
+struct Hypothesis {
+	Cell cell;
+	Row oldRows[64];
 };
-//Функция, возвращающая набор петель узла со словом str
-set<Conc> loop(string str) {
-	set<Conc> concs;
-	for (unsigned i = 0; i < str.size(); i++)
-		concs.insert(Conc(i, str[i]));
-	return concs;
+void printBytes(long long x, int length) {
+	for (int i = length; i > 0; i--) {
+		cout << ((x >> (i - 1) & 1) == 1 ? "#" : " ");
+		x -= ((x >> (i - 1)) << (i - 1));
+	}
 }
-
-void print(set<string> ss) {
-	for (set<string>::iterator it = ss.begin(); it != ss.end(); it++) 
-		cout << "  " << *it << endl;
-}
-//Процедура, печатающая самый жёсткий запрос, соответствующий данному узлу
-void print(Node *node) {
-	set<Conc> loop = node->loops;
-	for (unsigned i = 0; i < node->words.begin()->size(); i++)
-		loop.insert(Conc(i, '?'));
-	for (set<Conc>::iterator it = node->loops.begin(); it != node->loops.end(); it++)
-		loop.erase(Conc(it->number, '?'));
-	for (set<Conc>::iterator it = loop.begin(); it != loop.end(); it++)
-		cout << it->letter;
-	cout << endl;
-}
-
-class Voc {
-
-private:
-	//Набор словарей (каждый содержит слова с одинаковым числом букв), в каждом из которых хранятся указатели на узлы 
-	vector<vector<Node*>> nodes;
-	//Проверка, содержится ли в i-узле m-словаря набор слов words 
-	bool contains(set<string> words, int m, int i) {
-		bool b = true;
-		for (set<string>::iterator it = words.begin(); it != words.end(); it++)
-			if (nodes[m][i]->words.find(*it) == nodes[m][i]->words.end()) {
-				b = false;
-				break;
+Row replenish(int a[], int length, Row row, int size) {
+	Row r { row.spot, row.info };
+	long long one = 1;
+	long long t = (one << size) - 1;
+	long long p = 0;
+	for (long long l = (one << length) - 1; l <= ((one << (length)) - 1) << a[0]; l = (l + (l & -l)) | ((l ^ l + (l & -l)) >> 2) / (l & -l)) {
+		long long e = 0;
+		int i = 1;
+		for (int j = length + a[0]; j >= 0; j--) {
+			if (((l >> j) & 1) == 0) 
+				e <<= 1;
+			else {
+				e = (e << a[i]) + (one << a[i]) - 1;
+				if (i < length)
+					e <<= 1;
+				i++;
 			}
-		return b;
+		}
+		if (((e & r.info) ^ (r.spot & r.info)) == 0) {
+			t &= e;
+			p |= e;
+		}
 	}
-	//Функция, возвращающая nullptr или указатель на вершину m-словаря, содержащую только слова words, если такая есть
-	Node *exist(set<string> words, int m) {
-		for (unsigned i = 0; i < nodes[m].size(); i++)
-			if (nodes[m][i]->words.size() == words.size()) 
-				if (contains(words, m, i))
-					return nodes[m][i];
-		return nullptr;
+	r.spot = (r.spot | t) & p;
+	r.info = (r.info | t) | ((~p) & ((one << size) - 1));
+	return r;
+}
+Row replace(Row r, int x, int i, int size) {
+	r.spot = ((((r.spot >> (size - x)) << 1) + i) << (size - x - 1)) + (r.spot & (((long long)1 << (size - x - 1)) - 1));
+	r.info = ((((r.info >> (size - x)) << 1) + 1) << (size - x - 1)) + (r.info & (((long long)1 << (size - x - 1)) - 1));
+	return r;
+}
+bool isFinished(Row r[64], int width, int height) {
+	for (int i = 0; i < height; i++)
+	if (r[i].info != (((long long)1 << width) - 1))
+		return false;
+	return true;
+}
+Cell getNextCell(Row r[64], int width, int height) {
+	for (int i = 0; i < height; i++)
+	for (int j = 0; j < width; j++)
+	if (((r[i].info >> j) & 1) == 0) {
+		Cell c { width - j - 1, i };
+		return c;
 	}
-	//Функция, возвращающая nullptr или указатель на вершину, содержащую только слова words и слово str, если такая есть
-	Node *exist(set<string> words, string str) {
-		int m = str.size();
-		for (unsigned i = 0; i < nodes[m].size(); i++)
-			if (nodes[m][i]->words.size() == words.size() + 1 && nodes[m][i]->words.find(str) != nodes[m][i]->words.end()) 
-				if (contains(words, m, i))
-					return nodes[m][i];
-		return nullptr;
+	Cell c { -1, -1 };
+	return c;
+}
+void copyArray(Row row[], Row copy[], int size) {
+	for (int i = 0; i < size; i ++) {
+		copy[i].spot = row[i].spot;
+		copy[i].info = row[i].info;
 	}
-	//Функция, возвращающая nullptr или указатель на вершину, содержащую только слово str, если такая есть
-	Node *exist(string str) {
-		int m = str.size();
-		for (unsigned i = 0; i < nodes[m].size(); i++)
-			if (nodes[m][i]->words.size() == 1 && nodes[m][i]->words.find(str) != nodes[m][i]->words.end())
-				return nodes[m][i];
-		return nullptr;
-	}
-	//Процедура, удаляющая слово str из узлов. Обход осуществляется с помощью вектора vNode
-	void check(vector<Node*> vNode, string str) {
-		int m = str.size();
-		while (vNode.size() > 0) {
-			int n = vNode.size() - 1;
+}
+int main() {
+	int time = clock();
+	setlocale(LC_ALL, "rus");
+	long long hypothesis = 0;
+	int width = 0;
+	int height = 0;
+	int w[64][32];
+	int h[64][32];
+	int wSize[64];
+	int hSize[64];
+	bool isReplenished = true;
+	bool hasError = false;
+	int level = 0;
+	Hypothesis hyp[64];
 
-			set<string> words = vNode[n]->words;
-			words.erase(str);
-			Node *node = exist(words, str.size());
+	ifstream fin("123.txt"); 
 
-			if (node != nullptr) {
-				for (int i = 0; i < n; i++) {
-					set<Rib> ribs = vNode[i]->ribs;
-					for (set<Rib>::iterator it = ribs.begin(); it != ribs.end(); it++)
-						if (it->node == vNode[n]) {
-							vNode[i]->ribs.erase(*it);
-							vNode[i]->ribs.insert(Rib(it->conc, node));
-						}
+	fin >> height; 
+	for (int i = 0; i < height; i++) {
+		fin >> wSize[i];
+		for (int j = 1; j <= wSize[i]; j++)
+			fin >> w[i][j];
+	}
+	fin >> width;
+	for (int i = 0; i < width; i++) {
+		fin >> hSize[i];
+		for (int j = 1; j <= hSize[i]; j++)
+			fin >> h[i][j];
+	}
+	fin.close();
+
+	for (int i = 0; i < height; i++) {
+		w[i][0] = width;
+		for (int j = 1; j <= wSize[i]; j++) {
+			w[i][0] -= w[i][j] + 1;
+		}
+		w[i][0]++;
+	}
+	for (int i = 0; i < width; i++) {
+		h[i][0] = height;
+		for (int j = 1; j <= hSize[i]; j++) {
+			h[i][0] -= h[i][j] + 1;
+		}
+		h[i][0]++;
+	}
+	
+	long long wChanges = ((long long) 1 << height) - 1;
+	long long hChanges = ((long long) 1 << width) - 1;
+	Row row[64];
+	for (int i = 0; i < height; i++) {
+		row[i].info = 0;
+		row[i].spot = 0;
+	}
+
+	while (isReplenished) {
+		isReplenished = false;
+		for (int i = 0; i < height; i++) {
+			if ((((wChanges >> (height - i - 1)) & 1) == 1)) {
+				Row r = replenish(w[i], wSize[i], row[i], width);
+				if (r.spot == 0 && r.info == ((long long)1 << width) - 1) {
+					hasError = true;
+					break;
 				}
-				for (unsigned i = 0; i < nodes[m].size(); i++)
-					if (nodes[m][i] == vNode[n]) {
-						delete vNode[n];
-						nodes[m].erase(nodes[m].begin() + i);
+				for (int k = 0; (r.info >> k) != (row[i].info >> k); k++)
+					if (((r.info >> k) & 1) != ((row[i].info >> k) & 1))
+						hChanges |= (long long)1 << k;
+				if (r.info != row[i].info) {
+					isReplenished = true;
+					row[i] = r;
+				}
+			}
+		}
+		wChanges = 0;
+		if (!hasError) {
+			for (int i = 0; i < width; i++) {
+				if ((((hChanges >> i) & 1) == 1)) {
+					Row rowi;
+					rowi.spot = (long long)0;
+					rowi.info = (long long)0;
+					for (int j = 0; j < height; j++) {
+						rowi.spot <<= 1;
+						rowi.spot += (row[j].spot >> i) & 1;
+						rowi.info <<= 1;
+						rowi.info += (row[j].info >> i) & 1;
+					}
+					Row r = replenish(h[width - i - 1], hSize[width - i - 1], rowi, height);
+					if (r.spot == 0 && r.info == ((long long)1 << height) - 1) {
+						hasError = true;
 						break;
 					}
-			} else {
-				vNode[n]->words.erase(str);
-				set<string> words = vNode[n]->words;
-				for (unsigned i = 0; i < words.begin()->size(); i++) {
-					bool b = true;
-					for (set<string>::iterator it = words.begin(); it != words.end(); it++)
-						if ((*it)[i] != (*words.begin())[i]) {
-							b = false;
-							break;
-						}
-					if (b)
-						vNode[n]->loops.insert(Conc(i, (*words.begin())[i]));
-				}
-			}
-			vNode.pop_back();
-		}
-	}
-	//Функция, добавляющая слово str в узлы (рекурсивно), создающая новые узлы (если нужно) и возвращающая указатель на промежуточный корень
-	Node *add(Node *node, string str) {
-		set<Rib> brown, green = node->ribs; //brown/green - рёбра, идущие в узлы, в который нужно будет/нет довавить str
-		set<Conc> blue, yellow = node->loops, violet = loop(str); //blue - пересечение множества петель узла с loop(str),
-		//yellow - разность множества петель узла и blue, violet - разность loop(str) и blue		
-		set<Conc> help1;
-		for (set<Conc>::iterator it = violet.begin(); it != violet.end(); it++) 
-			if (yellow.find(*it) != yellow.end()) {
-				help1.insert(*it);
-				yellow.erase(*it);
-				blue.insert(*it);
-			}
-		set<Rib> help2;
-		for (set<Rib>::iterator it = green.begin(); it != green.end(); it++)
-			if (violet.find(it->conc) != violet.end()) {
-				help1.insert(it->conc);
-				help2.insert(*it);
-				brown.insert(*it);
-			}
-		for (set<Conc>::iterator it = help1.begin(); it != help1.end(); it++)
-			violet.erase(*it);
-		for (set<Rib>::iterator it = help2.begin(); it != help2.end(); it++)
-			green.erase(*it);
-
-		Node *_node;
-		if (yellow.size() == 0)
-			_node = node;
-		else
-			_node = new Node();
-
-		_node->words = node->words;
-		_node->words.insert(str);
-		_node->loops = blue;
-
-		_node->ribs.insert(green.begin(), green.end());
-		if (yellow.size() != 0)
-			for (set<Conc>::iterator it = yellow.begin(); it != yellow.end(); it++)  
-				_node->ribs.insert(Rib(*it, node));
-
-		Node *strNode = exist(str);
-		if (strNode == nullptr) {
-			strNode = new Node(str);
-			nodes[str.size()].push_back(strNode);
-		}
-		for (set<Conc>::iterator it = violet.begin(); it != violet.end(); it++)
-			_node->ribs.insert(Rib(*it, strNode));
-
-		for (set<Rib>::iterator it = brown.begin(); it != brown.end(); it++) 
-			if (it->node->words.find(str) == it->node->words.end()) {
-				Node *__node = exist(it->node->words, str);
-				_node->ribs.erase(Rib(it->conc, nullptr));
-				if (__node != nullptr) 
-					_node->ribs.insert(Rib(it->conc, __node));
-				else 
-					_node->ribs.insert(Rib(it->conc, add(it->node, str)));
-			}
-
-		if (exist(_node->words, str.size()) == nullptr)
-			nodes[str.size()].push_back(_node);
-		return _node;
-	}
-	//Сортировка в порядке убывания
-	void sort(vector<Node*> vNode) {
-		for (unsigned i = 0; i < vNode.size() - 1; i++)
-			if (vNode[i]->words.size() < vNode[i + 1]->words.size()) {
-				Node *node = vNode[i];
-				vNode[i] = vNode[i + 1];
-				vNode[i + 1] = node;
-				i = 0;
-			}
-	}
-
-public:
-	//Создание 31 словаря (слова, длиннее 30 букв почти не встречаются)
-	Voc() {
-		for (int i = 0; i <= 30; i++)
-			nodes.push_back(vector<Node*>());
-	}
-
-	void add(string str) {
-		int m = str.size();
-		if (nodes[m].size() == 0) {
-			nodes[m].push_back(new Node(str));
-		} else {
-			if (nodes[m][0]->words.find(str) != nodes[m][0]->words.end())
-				cout << "Already have it" << endl;
-			else {
-				Node *node = add(nodes[m][0], str);
-				if (nodes[m][0] != node) {
-					nodes[m].insert(nodes[m].begin(), node);
-					nodes[m].pop_back();
-				}
-			}
-		}
-	}
-
-	void del(string str) {
-		int m = str.size();
-		if (nodes[m].size() > 0 && nodes[m][0]->words.find(str) != nodes[m][0]->words.end()) {
-
-			vector<Node*> vNode;
-			vNode.push_back(nodes[m][0]);
-
-			while (true) {
-				if (vNode.size() == 0)
-					break;
-				int m = str.size();
-				set<Conc> strLoop = loop(str);
-				for (set<Rib>::iterator it = vNode[0]->ribs.begin(); it != vNode[0]->ribs.end(); it++)
-					if (it->node->words.size() == 1 && it->node->words.find(str) == it->node->words.begin()) {
-						vNode[0]->ribs.erase(*it);
-						it = vNode[0]->ribs.begin();
-					}
-
-				for (set<Rib>::iterator it = vNode[0]->ribs.begin(); it != vNode[0]->ribs.end(); it++)
-					if (strLoop.find(it->conc) != strLoop.end())
-						vNode.push_back(it->node);
-
-				vNode.erase(vNode.begin());
-			}
-
-			for (unsigned i = 0; i < nodes[m].size(); i++)
-				if (nodes[m][i]->words.find(str) != nodes[m][i]->words.end())
-					if (nodes[m][i]->words.size() == 1) {
-						delete nodes[m][i];
-						nodes[m].erase(nodes[m].begin() + i);
-						i--;
-					} else
-						vNode.push_back(nodes[m][i]);
-			if (vNode.size() > 0)
-				sort(vNode);
-
-			check(vNode, str);
-		} 
-		else
-			cout << "Not exist" << endl;
-	}
-
-	void find(string str) {
-		int m = str.size();
-		if (nodes[m].size() > 0) {
-			Node *node = nodes[m][0];
-			for (int i = 0; i < m; i++) {
-				if (str[i] != '?') {
-					Conc conc = Conc(i, str[i]);
-					set<Conc>::iterator ik = node->loops.find(conc);
-					if (ik == node->loops.end()) {
-						set<Rib>::iterator ir = node->ribs.find(Rib(conc, nullptr));
-						if (ir != node->ribs.end())
-							node = ir->node;
-						else {
-							cout << "Not exist" << endl;
-							break;
+					for (int k = 0; (r.info >> k) != (rowi.info >> k); k++)
+					if (((r.info >> k) & 1) != ((rowi.info >> k) & 1))
+						wChanges |= (long long)1 << k;
+					if (r.info != rowi.info) {
+						isReplenished = true;
+						for (int j = height - 1; j >= 0; j--) {
+							row[j].spot = ((((row[j].spot >> (i + 1)) << 1) + ((r.spot >> (height - j - 1)) & 1)) << i) + (row[j].spot & (((long long)1 << i) - 1));
+							row[j].info = ((((row[j].info >> (i + 1)) << 1) + ((r.info >> (height - j - 1)) & 1)) << i) + (row[j].info & (((long long)1 << i) - 1));
 						}
 					}
 				}
-				if (i == m - 1) {
-					cout << "----find :" << endl;
-					print(node->words);
-					cout << "----------" << endl;
-				}
 			}
-		} else
-			cout << "Vocabulary is empty" << endl;
-	}
-	//Печать словарей
-	void pr() {
-		for (int m = 1; m <= 30; m++) 
-			for (unsigned i = 0; i < nodes[m].size(); i++)
-				print(nodes[m][i]);
-	}
-};
-
-int main() {
-
-	Voc voc;
-	string str;
-
-	cout << "////////////////Instruction/////////////////" << endl;
-	cout << "Keyboard commands:" << endl;
-	cout << "  a - to add word" << endl;
-	cout << "  d - to delete word" << endl;
-	cout << "  f - to find word" << endl;
-	cout << "  p - to print vocabulary" << endl;
-	cout << "  e - to exit" << endl;
-	cout << "All input words must be from 1 to 30 letters" << endl;
-	cout << "////////////////////////////////////////////" << endl;
-
-	while (true) {
-		cout << "Press letter: ";
-		cin >> str;
-		if (str == "e")
-			break;
-		if (str == "a") {
-			cout << "Enter the word to add" << endl;
-			cin >> str;
-			voc.add(str);
 		}
-		else if (str == "d") {
-			cout << "Enter the word to delete" << endl;
-			cin >> str;
-			voc.del(str);
+		hChanges = 0;
+		if (hasError) {
+			if (level == 0)
+				break;
+			hasError = false;
+			while ((hypothesis & 1) == 0) {
+				hypothesis >>= 1;
+				level--;
+			}
+			hypothesis--;
+			copyArray(hyp[level - 1].oldRows, row, height);
+			row[hyp[level - 1].cell.y] = replace(row[hyp[level - 1].cell.y], hyp[level - 1].cell.x, 0, width);
+			wChanges |= (long long)1 << (height - hyp[level - 1].cell.y - 1);
+			hChanges |= (long long)1 << (width - hyp[level - 1].cell.x - 1);
+			if (hypothesis == 0)
+				level = 0;
+			isReplenished = true;
 		}
-		else if (str == "f") {
-			cout << "Enter the word (with question marks) to find" << endl;
-			cin >> str;
-			voc.find(str);
-		}
-		else if (str == "p") {
-			cout << "Current nodes (designated as the most tight requests):" << endl;
-			voc.pr();
+		else if (!isReplenished && !isFinished(row, width, height)) {
+			hypothesis = (hypothesis << 1) + 1;
+			Cell cell = getNextCell(row, width, height);
+			level++;
+			hyp[level - 1].cell = cell;
+			copyArray(row, hyp[level - 1].oldRows, height);
+			row[cell.y] = replace(row[cell.y], cell.x, 1, width);
+			isReplenished = true;
+			wChanges |= (long long)1 << (height - cell.y - 1);
+			hChanges |= (long long)1 << (width - cell.x - 1);
 		}
 	}
+	int t = clock() - time;
+	cout << "program duration = " << (t / 1000 / 60) << " m " << (t / 1000 % 60) << " s " << (t % 1000) << " ms" << endl;
+	if (!hasError) {
+		cout << endl;
+		for (int i = 0; i < height; i++) {
+			printBytes(row[i].spot, width);
+			cout << endl;
+		}
+	} else
+		cout << "Has error";
+	cout << endl;
 	return EXIT_SUCCESS;
 }
